37ae018c8145a2e1e1e21d4e851f552b548a8054
Joao Victor <joao.vr.fernandes@gmail.com>
2025-03-25 17:02:20 -0300

    criação das pastas Control e Model

--- stock_project/src/Control/server.js
+++ stock_project/src/Control/server.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import cors from 'cors';
+//importa as funcoes do database.js
+import {} from './database.js'
+
 const app = express();
 const corsOptions = {
     origin:  ["http://localhost:5173"],
 
 };
+//error handling
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Alguma coisa quebrou!')
+})
 
 app.use(cors(corsOptions));
 
@@ -12,7 +20,8 @@ app.get("/api", (req, res) => {
     res.json({fruits: ["apple", "orange", "banana"]})
 });
 
-const port = 8080
+//http:://localhost:PORT
+const port = 8080 //http:://localhost:8080
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
