7af7421c24ac3467920b829fed8f09b23f3bff61
Joao Victor <joao.vr.fernandes@gmail.com>
2025-03-25 18:31:45 -0300

    Selects feitos

--- stock_project/src/Model/database.js
+++ stock_project/src/Model/database.js
@@ -8,14 +8,64 @@ const pool = mysql.createPool({
 
 }).promise()
 
-async function getProduto(){
-    const [rows] = await pool.query(``)
+async function getProduto(id_produto){
+    const [rows] = await pool.query(`
+        SELECT nome, quantidade_disponivel, quantidade_pack, ultima_atualizacao
+        FROM Produto
+        WHERE id_produto = ?
+        `, [id])
     return rows
 }
 
-async function getTotalProduto(){
-    const [rows] = await pool.query(``)
+async function getTotalProduto(id_produto){
+    const [rows] = await pool.query(`
+        SELECT quantidade_disponivel
+        FROM Produto
+        WHERE id_produto = ?
+        `, [id])
     return rows
 }
 
+async function getPermissaoVisualizar(id_usuario){
+    const [rows] = await pool.query(`
+        SELECT permissao_estoque_visualizar
+        FROM Permissao
+        WHERE id_usuario = ?
+        `, [id])
+    return rows
+}
 
+async function getPermissaoEditar(id_usuario){
+    const [rows] = await pool.query(`
+        SELECT permissao_estoque_editar
+        FROM Permissao
+        WHERE id_usuario = ?
+        `, [id])
+    return rows
+}
+
+async function getMovimentacao(id) {
+    const [rows] = await pool.query(`
+        SELECT *
+        FROM Movimentacao
+        WHERE id_produto = ?
+        `, [id])
+    return rows
+}
+
+async function getHistorico_Alteracoes_Usuario(id) {
+    const [rows] = await pool.query(`
+        SELECT *
+        FROM Historico_Alteracoes_Usuario
+        WHERE id_usuario = ?
+        `, [id])
+    return rows
+}
+
+async function insertUsuario(nome, email){
+    const result = await pool.query(`
+        INSERT INTO Usuario (nome, email, status)
+        VALUES (?, ?, ativo)
+        `, [nome, email])
+    return result
+}
