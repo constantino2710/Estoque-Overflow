463bef610b29dc10591505d22192c17d6949a20a
pedrohasantos <133005154+pedrohasantos@users.noreply.github.com>
2025-04-02 02:13:17 -0300

    correção banco de dados

--- stock_project/src/Model/schema.sql
+++ stock_project/src/Model/schema.sql
@@ -1,5 +1,4 @@
-USE  Banco;
-
+USE Banco
 -- Tabela: Produto
 CREATE TABLE Produto (
     id_produto SERIAL PRIMARY KEY,
@@ -23,7 +22,7 @@ CREATE TABLE Usuario (
 -- Tabela: Permissao
 CREATE TABLE Permissao (
     id_permissao SERIAL PRIMARY KEY,
-    id_usuario INTEGER NOT NULL UNIQUE,
+    id_usuario BIGINT UNSIGNED NOT NULL UNIQUE,
     permissao_estoque_visualizar BOOLEAN NOT NULL DEFAULT FALSE,
     permissao_estoque_editar BOOLEAN NOT NULL DEFAULT FALSE,
     FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario) ON DELETE CASCADE
@@ -32,8 +31,8 @@ CREATE TABLE Permissao (
 -- Tabela: Movimentacao
 CREATE TABLE Movimentacao (
     id_movimentacao SERIAL PRIMARY KEY,
-    id_produto INTEGER NOT NULL,
-    id_usuario INTEGER NOT NULL,
+    id_produto BIGINT UNSIGNED NOT NULL,
+    id_usuario BIGINT UNSIGNED NOT NULL,
     quantidade_movimentada INTEGER NOT NULL,
     tipo_movimentacao VARCHAR(20) CHECK (tipo_movimentacao IN ('retirada', 'devolucao')) NOT NULL,
     data_hora TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -45,10 +44,10 @@ CREATE TABLE Movimentacao (
 -- Tabela: Historico_Alteracoes_Usuario
 CREATE TABLE Historico_Alteracoes_Usuario (
     id_historico SERIAL PRIMARY KEY,
-    id_usuario INTEGER NOT NULL,
+    id_usuario BIGINT UNSIGNED NOT NULL UNIQUE,
     alteracao TEXT NOT NULL,
     data_hora TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    alterado_por INTEGER NOT NULL,
+    alterado_por BIGINT UNSIGNED NOT NULL,
     FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario),
     FOREIGN KEY (alterado_por) REFERENCES Usuario(id_usuario)
 );
@@ -56,17 +55,16 @@ CREATE TABLE Historico_Alteracoes_Usuario (
 -- Tabela: Historico_Produto
 CREATE TABLE Historico_Produto (
     id_historico_produto SERIAL PRIMARY KEY,
-    id_produto INTEGER NOT NULL,
+    id_produto BIGINT UNSIGNED NOT NULL UNIQUE,
     alteracao TEXT NOT NULL,
     quantidade_antiga INTEGER,
     quantidade_nova INTEGER,
     data_hora TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    alterado_por INTEGER NOT NULL,
+    alterado_por BIGINT UNSIGNED NOT NULL ,
     FOREIGN KEY (id_produto) REFERENCES Produto(id_produto),
     FOREIGN KEY (alterado_por) REFERENCES Usuario(id_usuario)
 );
 
--- criacao de roles
 CREATE ROLE admin;
 CREATE ROLE usuario;
 
@@ -87,4 +85,3 @@ GRANT usuario TO 'comum_user'@'localhost';
 SET DEFAULT ROLE admin FOR 'admin_user'@'localhost';
 SET DEFAULT ROLE usuario FOR 'comum_user'@'localhost';
 
-CREATE TABLE Produto (     id_produto SERIAL PRIMARY KEY,     nome VARCHAR(100) NOT NULL,     quantidade_disponivel INTEGER NOT NULL DEFAULT 0,     quantidade_pack INTEGER NOT NULL DEFAULT 24,     ultima_atualizacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,     limite_minimo INTEGER NOT NULL DEFAULT 0,     tipo VARCHAR(20) CHECK (tipo IN ('OPME', 'pack')) NOT NULL )
