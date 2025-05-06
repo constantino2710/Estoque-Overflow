e69bb5aaff0320c532b01f7880d3cdebe0d8cb8f
constantino2710 <constantinodev27@gmail.com>
2025-05-06 11:04:07 -0300

    v2

--- stock_project/src/pages/dashboardComponents/stockedItems.tsx
+++ stock_project/src/pages/dashboardComponents/stockedItems.tsx
@@ -36,7 +36,7 @@ export function StockedItems() {
   }, []);
 
   return (
-    <div className="flex flex-col gap-4 w-full h-full bg-[var(--gray-800)] rounded-xl p-4 overflow-auto border border-[var(--gray-200)]">
+    <div className="flex flex-col gap-4 w-full h-full bg-[var(--gray-800)] rounded-xl p-4 overflow-auto border border-[var(--gray-200)] scrollbar-hide">
       <h2 className="text-2xl text-white font-bold">Estoque Atual</h2>
 
       {loading && <p className="text-white">Carregando ocupação...</p>}
